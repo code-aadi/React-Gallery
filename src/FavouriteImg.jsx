96ff755f0f905800d120023e7abfb554f27f332b
code-aadi <kouravaditya7@gmail.com>
2026-03-18 17:48:52 +0530

    Add empty state message for favourites page

--- src/FavouriteImg.jsx
+++ src/FavouriteImg.jsx
@@ -1,40 +1,44 @@
 import React from 'react'
 
-const FavouriteImg = ({data , setFavourite}) => {
-    function removeFromFav(pic){
-         let filteredImg =  data.filter((item)=> item.id !== pic.id )
-   setFavourite(filteredImg)
-    }
+const FavouriteImg = ({ data, setFavourite }) => {
+
+  function removeFromFav(pic) {
+    let filteredImg = data.filter((item) => item.id !== pic.id)
+    setFavourite(filteredImg)
+  }
+
   return (
- <>
- {data.map((pic) => {
-     return (
-         <div className="image-card" key={pic.id}>
-      
-      <img 
-        className='favourite' 
-        src="delete-symbol.png" 
-        alt="fav" 
-        onClick={()=>{
-            removeFromFav(pic)
-        }}
-        />
+    <>
+      {!data || data.length === 0 ? (
+        <h2 className="no-fav">No favourites added yet ❤️</h2>
+      ) : (
+        data.map((pic) => {
+          return (
+            <div className="image-card" key={pic.id}>
 
-      
-      <a target='_blank' href={pic.url} rel="noreferrer">
-        <img 
-          className='gallery-img' 
-          src={`https://picsum.photos/id/${pic.id}/300/200`} 
-          alt={pic.author} 
-          />
-        <h2 className='authorName'>{pic.author}</h2>
-      </a>
-    </div>
-  );
-})}
-  
-</>   
+              <img 
+                className='favourite' 
+                src="delete-symbol.png" 
+                alt="fav" 
+                onClick={() => {
+                  removeFromFav(pic)
+                }}
+              />
 
+              <a target='_blank' href={pic.url} rel="noreferrer">
+                <img 
+                  className='gallery-img' 
+                  src={`https://picsum.photos/id/${pic.id}/300/200`} 
+                  alt={pic.author} 
+                />
+                <h2 className='authorName'>{pic.author}</h2>
+              </a>
+
+            </div>
+          );
+        })
+      )}
+    </>
   )
 }
 
